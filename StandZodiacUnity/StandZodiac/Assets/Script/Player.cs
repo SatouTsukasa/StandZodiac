a09492953f9475e5ed3cba45443cd3fa1ccd23a1
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2019-03-15 09:51:02 +0900

    update13

--- StandZodiacUnity/StandZodiac/Assets/Script/Player.cs
+++ StandZodiacUnity/StandZodiac/Assets/Script/Player.cs
@@ -6,20 +6,41 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    // PlayerBulletプレハブ
+    public GameObject bullet;
+
+    //public GameObject player;
+
+    private Vector3 playerPos;
+    private Vector3 mousePos;
     // Start is called before the first frame update
-    void Start()
+
+    // Startメソッドをコルーチンとして呼び出す
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Instantiate(bullet, transform.position, transform.rotation);
+            // 0.05秒待つ
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    /*void Start()
     {
         
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        //画面外判定
+        if (!GetComponent<SpriteRenderer>().isVisible)
+        {
+            Debug.Log("画面外");
+        }
 
-    private void FixedUpdate()
-    {
         // エディタ、実機で処理を分ける
 
         if (Application.isEditor)
@@ -28,16 +49,37 @@ public class Player : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 Debug.Log("クリックした瞬間");
+                /*Vector3 position = Input.mousePosition;
+                iTween.MoveTo(this.gameObject, iTween.Hash("x", position.x, "y", position.y, "time", 1.0f));*/
+
+                playerPos = this.transform.position;
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
 
             if (Input.GetMouseButtonUp(0))
             {
+                playerPos = Vector3.zero;
+                mousePos = Vector3.zero;
                 Debug.Log("離した瞬間");
             }
 
             if (Input.GetMouseButton(0))
             {
+                /*Vector3 position = Input.mousePosition;
+                iTween.MoveTo(this.gameObject, iTween.Hash("x", position.x, "y", position.y, "time", 1.0f));*/
                 Debug.Log("クリックしっぱなし");
+
+                Vector3 prePos = this.transform.position;
+                Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mousePos;
+
+                //タッチ対応デバイス向け、1本目の指にのみ反応
+                if (Input.touchSupported)
+                {
+                    diff = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position) - mousePos;
+                }
+
+                diff.z = 0.0f;
+                this.transform.position = playerPos + diff;
             }
         }
         else
@@ -65,6 +107,11 @@ public class Player : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 
 }
