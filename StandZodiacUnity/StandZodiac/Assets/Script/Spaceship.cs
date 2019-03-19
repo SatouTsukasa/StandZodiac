3a02282dace9440c0a8cc4a61aa0afe0187018c1
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2019-03-19 10:41:58 +0900

    update14

--- StandZodiacUnity/StandZodiac/Assets/Script/Spaceship.cs
+++ StandZodiacUnity/StandZodiac/Assets/Script/Spaceship.cs
@@ -2,8 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Rigidbody2Dコンポーネントを必須にする
+[RequireComponent(typeof(Rigidbody2D))]
 public class Spaceship : MonoBehaviour
 {
+    // 移動スピード
+    public float speed;
+
+    // 弾を撃つ間隔
+    public float shotDelay;
+
+    // 弾のPrefab
+    public GameObject bullet;
+
+    // 弾を撃つかどうか
+    public bool canShot;
+
+    // 爆発のPrefab
+    public GameObject explosion;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +31,23 @@ public class Spaceship : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // 爆発の作成
+    public void Explosion()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+
+    // 弾の作成
+    public void Shot(Transform origin)
+    {
+        Instantiate(bullet, origin.position, origin.rotation);
+    }
+
+    // 機体の移動
+    public void Move(Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
     }
 }
