d509bfecff9f4526a694d8e00c22627ff70fcb29
missingno0083 <43591807+missingno0083@users.noreply.github.com>
2019-04-03 13:30:30 +0900

    ゲームオーバーの処理、テキスト

---
+++ StandZodiacUnity/StandZodiac/Assets/Script/MainPart/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GameOver : MonoBehaviour
+{
+
+    public GameObject player_ob;
+    public Text Gameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player_ob == null) {
+
+            Gameover.SetActive(true);
+
+        }
+
+    }
+}
