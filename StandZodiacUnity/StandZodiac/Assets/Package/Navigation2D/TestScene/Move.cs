a9f2458357366eb96ac2553ef14378a48edaaf0a
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2019-04-11 12:49:47 +0900

    update34
    更新34回目

---
+++ StandZodiacUnity/StandZodiac/Assets/Package/Navigation2D/TestScene/Move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    public GameObject Player;
+    void Update()
+    {
+        if (Player.transform.position.y < transform.position.y)
+        {
+            //Vector3 w = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 w = Player.transform.position;
+            GetComponent<NavMeshAgent2D>().destination = w;
+        }
+        else
+        {
+            Debug.Log("rrrrrrrrrrrrrrrrr");
+            GetComponent<Rigidbody2D>().velocity = transform.up * -1 * 
+                GetComponent<Enemy>().speed * GetComponent<Spaceship>().speed;
+        }
+    }
+}
