392e12917927cbc6158acfc25d881bb37a099155
SagamiKazuki <47546532+SagamiKazuki@users.noreply.github.com>
2019-04-23 09:50:15 +0900

    修正途中

--- StandZodiacUnity/StandZodiac/Assets/Script/MainPart/Gauge.cs
+++ StandZodiacUnity/StandZodiac/Assets/Script/MainPart/Gauge.cs
@@ -7,12 +7,14 @@ using UnityEngine.UI;
 public class Gauge : MonoBehaviour {
 
     public GameObject barrier;
-    
 
+    bool dec_Flag = false;
+
+    float oneTime;
     float n_gauge = 0;
     float gaugeMAX = 1;
 
-    float addGauge = 0.01f;
+    float addGauge = 0.1f;
 
     GameObject special_Gauge;
     
@@ -24,13 +26,24 @@ public class Gauge : MonoBehaviour {
     
     void Update()
     {
-        ADDgauge();
+        //ADDgauge();
+        
+        if (dec_Flag == true)
+        {
+            //ゲージの初期化
+            n_gauge -= 0.001666667f;
+            this.special_Gauge.GetComponent<Image>().fillAmount = n_gauge;
+            if (n_gauge == 0)
+            {
+                dec_Flag = false;
+            }
+        }
     }
 
     //敵に攻撃したときに呼び出す
     public void ADDgauge()
     {
-        if (n_gauge <= gaugeMAX) {
+        if (n_gauge <= gaugeMAX && dec_Flag == false) {
             n_gauge = n_gauge + addGauge;
             this.special_Gauge.GetComponent<Image>().fillAmount += addGauge;
         }
@@ -47,9 +60,7 @@ public class Gauge : MonoBehaviour {
             GameObject sc_barrier = Instantiate(barrier);
             sc_barrier.transform.position = new Vector3(p_pos.x, p_pos.y, p_pos.z);
 
-            //ゲージの初期化
-            this.special_Gauge.GetComponent<Image>().fillAmount = 0;
-            n_gauge = 0;
+            dec_Flag = true;
         }
     }
 
