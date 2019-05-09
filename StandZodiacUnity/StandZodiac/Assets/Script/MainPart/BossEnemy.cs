14bbabd7f2254b0943a8c122add3cbc16f3310d1
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2019-05-09 14:01:36 +0900

    更新

--- StandZodiacUnity/StandZodiac/Assets/Script/MainPart/BossEnemy.cs
+++ StandZodiacUnity/StandZodiac/Assets/Script/MainPart/BossEnemy.cs
@@ -16,8 +16,10 @@ public class BossEnemy : MonoBehaviour
     public GameObject HutagoSister;
 
     private bool HutagoH;
+    bool mea_flg;
 
     ///HutagoSister用--------------
+    GameObject HSister;
     public bool HutagoS;
     //半径
     public float radius;
@@ -47,16 +49,57 @@ public class BossEnemy : MonoBehaviour
                     HutagoPower();
                     HutagoH = false;
                 }
-                transform.position = new Vector3(transform.position.x + (Mathf.Sin(Time.time * enemy.speed)), transform.position.y, 0);
+                //transform.position = new Vector3(transform.position.x + (Mathf.Sin(Time.time * enemy.speed) * 2f), transform.position.y, 0);
+
+                //ふたご座（男）の現在地
+                Vector2 ufo_pos = this.transform.position;
+
+                if (ufo_pos.x >= 640) mea_flg = true;
+
+                if (ufo_pos.x <= 80) mea_flg = false;
+
+                if (mea_flg)
+                {
+                    this.transform.position = new Vector2(ufo_pos.x - 5, ufo_pos.y);
+                }
+                else
+                {
+                    this.transform.position = new Vector2(ufo_pos.x + 5, ufo_pos.y);
+                }
+            }
+            if(enemy.hp <= 0)
+            {
+                //Destroy(GameObject.Find("EnemyHutagoSister"));
+                Destroy(HSister);
+            }
+        }
+        else
+        {
+            //ふたご座（男）の現在地
+            Vector2 ufo_pos = this.transform.position;
+
+            if (ufo_pos.x >= 640) mea_flg = true;
+
+            if (ufo_pos.x <= 80) mea_flg = false;
+
+            if (mea_flg)
+            {
+                this.transform.position = new Vector2(ufo_pos.x - 5, ufo_pos.y);
+            }
+            else
+            {
+                this.transform.position = new Vector2(ufo_pos.x + 5, ufo_pos.y);
             }
         }
 
         if (HutagoS == true)
         {
-            float x = transform.position.x + (Mathf.Cos(Time.time * enemy.speed) * radius);
-            float y = transform.position.y + (Mathf.Sin(Time.time * enemy.speed) * radius);
+            Transform HutagoT = GameObject.Find("EnemyHutago").transform;
+            float x = HutagoT.position.x + (Mathf.Cos(Time.time * enemy.speed) * radius);
+            float y = HutagoT.position.y + (Mathf.Sin(Time.time * enemy.speed) * radius);
             float z = 0f;
             transform.position = new Vector3(x, y, z);
+            
         }
 
     }
@@ -64,12 +107,12 @@ public class BossEnemy : MonoBehaviour
     void HutagoPower()
     {
         Debug.Log("asdfghj");
-        GameObject HSister = (GameObject)Instantiate(HutagoSister, new Vector2(transform.position.x, transform.position.y - 10), Quaternion.identity);
-        HSister.transform.parent = transform;
+        HSister = (GameObject)Instantiate(HutagoSister, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+        //HSister.transform.parent = transform;
         Transform Tf = HSister.GetComponent<Transform>();
         Vector3[] path =
         {
-                new Vector3(Tf.localPosition.x,Tf.localPosition.y * 1.1f,0f),
+                new Vector3(Tf.localPosition.x,Tf.localPosition.y * 0.9f,1f),
                 //new Vector3(0f,150f,0f),
             };
         //DOTweenを使ったアニメ作成
