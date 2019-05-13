ab027d45faa173b5143469e8a2601fab67a92b24
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2019-05-13 13:42:11 +0900

    更新

--- StandZodiacUnity/StandZodiac/Assets/Script/MainPart/Player.cs
+++ StandZodiacUnity/StandZodiac/Assets/Script/MainPart/Player.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-//using UnityStandardAssets.CrossPlatformInput;
 
 
 public class Player : MonoBehaviour
